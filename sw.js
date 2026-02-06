ab9e116418e18725b944f5677356ec954eb8afc9
Refivenine <hrangategamer@gmail.com>
2026-02-06 10:22:30 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'evtx-v2-pro';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://refivenine.github.io/Evtx/logo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Fetch Assets
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
